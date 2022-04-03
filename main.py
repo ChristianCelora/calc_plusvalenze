5104f6892fd2d93d12f05651654c70d223cb08a4
ChristianCelora <christian.celora@gmail.com>
2022-04-03 16:47:48 +0200

    added csv read. Updated gitignore

--- main.py
+++ main.py
@@ -4,11 +4,17 @@
         1 - filename .csv con le operazioni di importare
 """
 import sys
+import csv
 
 def main():
     if len(sys.argv) < 2:
         print("Specificare un file csv in input")
         sys.exit(1)
+
+    # Read operations
+    with open(sys.argv[1], "r") as csv_file:
+        reader = csv.reader(csv_file)
+
 
     sys.exit(0)
 
