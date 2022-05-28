75aeb88d3d95c04f1815afab8f6182da3fa5c9a3
ChristianCelora <christian.celora@gmail.com>
2022-05-29 00:22:36 +0200

    added tests on convert

--- tests/unit/test_plus_v_calculator.py
+++ tests/unit/test_plus_v_calculator.py
@@ -31,4 +31,31 @@ def test_BuySellProcessTransactions():
     assert len(t_dict["BTC"][Transaction.TYPE_SELL]) == 1
 
 def test_ConvertProcessTransactions():
-    pass
+    transactions = [
+        Transaction(
+            datetime(2022, 5, 29, 0, 0, 0), Transaction.TYPE_CONVERT, "BTC", 
+            0.003863, "USD", 19414.96, 15, "Converted 0,003863 BTC to 0,12126672 ETH"
+        )
+    ]
+
+    plusv_calc = PlusVCalculator()
+    t_dict = plusv_calc.processTransactions(transactions)
+
+    assert isinstance(t_dict, dict)
+    assert "BTC" in t_dict
+    assert "ETH" in t_dict
+    assert Transaction.TYPE_SELL in t_dict["BTC"]
+    assert Transaction.TYPE_BUY in t_dict["ETH"]
+
+    t_btc = t_dict["BTC"][Transaction.TYPE_SELL].popitem()
+    t_eth = t_dict["ETH"][Transaction.TYPE_BUY].popitem()
+    assert isinstance(t_btc, Transaction)
+    assert isinstance(t_eth, Transaction)
+    assert t_btc.qty == 0.003863
+    assert t_btc.asset_price == 0.003863
+
+    assert t_eth.qty == 0.12126672
+    # 19414.96 : 0.12126672 = x : 
+    # x = (19414.96 * 0.003863) / 0.12126672
+    assert t_eth.asset_price == 618.471337231 # in USD
+
