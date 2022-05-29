4735d945c9c7455273709d95c47e6a1c81eb9cf8
ChristianCelora <christian.celora@gmail.com>
2022-05-29 08:43:02 +0200

    refactoring PlusVCalculator class

--- PlusvCalc/PlusVCalculator.py
+++ PlusvCalc/PlusVCalculator.py
@@ -25,20 +25,29 @@ class PlusVCalculator:
             Formattazione lista transazioni in un dizionario.
             La sua struttura:
                 symbol -> bought / sold -> date -> Transaction
+            
+            Le transazioni di tipo "Convert" devono essere trasformate in 2 transazioni.
+            Una di vendita della moneta in questione e una di acquisto della nuova moneta
         """
         tr_dict = {}
         for t in t_list:
             # Filter unwanted typed
             if t.type != Transaction.TYPE_CONVERT and t.type != Transaction.TYPE_BUY and t.type != Transaction.TYPE_SELL: 
                 continue
-            
-            if t.asset not in tr_dict:
-                tr_dict[t.asset] = {}
 
-            if t.type not in tr_dict[t.asset]:
-                tr_dict[t.asset][t.type] = {}
-            
-            t_time = t.timestamp.strftime("%Y%m%dT%H%m%s")
-            tr_dict[t.asset][t.type][t_time] = t # tr_dict[t.asset] is ordered by date
+            tr_dict = self.__addTransactionToDict(tr_dict, t)
             
         return tr_dict
+
+    def __addTransactionToDict(self, dict: dict, t: Transaction) -> dict:
+        if t.asset not in dict:
+                dict[t.asset] = {}
+
+        if t.type not in dict[t.asset]:
+            dict[t.asset][t.type] = {}
+        
+        t_time = t.timestamp.strftime("%Y%m%dT%H%m%s")
+        # dict[t.asset] is ordered by date
+        dict[t.asset][t.type][t_time] = t 
+
+        return dict
